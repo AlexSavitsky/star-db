a166f19739fa5280d4667693d6e686d5d9809951
Alexandr <savasavitsky@gmail.com>
2022-06-06 14:53:24 +0300

    Aded functionality
    - loading spinner
    - error handler

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,18 +1,17 @@
-import React, {Component} from 'react'; 
-import './item-list.css';
+import React, { Component } from "react";
+import "./item-list.css";
 
-export default class ItemList extends Component{
-    render() {
-        return (
-            <div className="item-list">
-                <ul className="list-group">
-                    <li className="list-group-item">Name 1</li>
-                    <li className="list-group-item">Name 2</li>
-                    <li className="list-group-item">Name 3</li>
-                    <li className="list-group-item">Name 4</li>
-                </ul>
-            </div>
-        )
-    }
+export default class ItemList extends Component {
+  render() {
+    return (
+      <div className="item-list">
+        <ul className="list-group">
+          <li className="list-group-item list-group-item-action">Name 1</li>
+          <li className="list-group-item list-group-item-action">Name 2</li>
+          <li className="list-group-item list-group-item-action">Name 3</li>
+          <li className="list-group-item list-group-item-action">Name 4</li>
+        </ul>
+      </div>
+    );
+  }
 }
-
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-spinner-biwvcg0xmlf">
+      <div className="ldio-a7ibdxbx5bp">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+export default Spinner;
